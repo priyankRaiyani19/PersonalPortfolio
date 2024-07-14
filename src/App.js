1446b2b69d5bb3e233ee0427cb71b7668ebb0a25
priyank <priyankraiyani19@gmail.com>
2024-07-14 21:29:17 +0530

    working on About

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import NavBar from "./components/NavBar";
 import Home from "./components/Home";
 import SocialLinks from "./components/SocialLinks";
+import About from "./components/About";
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
             <NavBar/>
             <Home/>
             <SocialLinks/>
+            <About/>
 
         </div>
     );
--- src/components/SocialLinks.js
+++ src/components/SocialLinks.js
@@ -9,64 +9,48 @@ const SocialLinks = () => {
     const Slinks = [
 
         {
-            id: 1,
-            child: (
-                <>
-                    LinkedIn <FaLinkedin size={30}/>
-                </>
-            ),
-            href: 'https://www.linkedin.com/in/priyank-raiyani-702b61223',
-            style:'rounded-tr-md'
-        },
-        {
-            id: 2,
-            child: (
-                <>
-                    GitHub <FaGithub size={30}/>
-                </>
-            ),
-            href: 'https://github.com/priyankRaiyani19',
+            id: 1, child: (<>
+                LinkedIn <FaLinkedin size={30}/>
+            </>), href: 'https://www.linkedin.com/in/priyank-raiyani-702b61223', style: 'rounded-tr-md'
+        }, {
+            id: 2, child: (<>
+                GitHub <FaGithub size={30}/>
+            </>), href: 'https://github.com/priyankRaiyani19',
 
-        },
-        {
-            id: 3,
-            child: (
-                <>
-                    Mail <HiOutlineMail size={30}/>
-                </>
-            ),
-            href: 'https://www.linkedin.com/in/priyank-raiyani-702b61223',
+        }, {
+            id: 3, child: (<>
+                Mail <HiOutlineMail size={30}/>
+            </>),
+            href: 'mailto:priyankraiyani1984@gmail.com',
 
-        },
-        {
-            id: 1,
-            child: (
-                <>
-                 Resume <BsFillPersonLinesFill size={30}/>
-                </>
-            ),
-            href: "../Resume_PR.pdf",
-            style:'rounded-br-md',
-            download:true,
-        }
-    ]
+        }, {
+            id: 1, child: (<>
+                Resume <BsFillPersonLinesFill size={30}/>
+            </>), href: "/Resume_PR.pdf", style: 'rounded-br-md', download: true,
+        }]
 
 
-    return (
-        <div className="flex flex-col top-[35%] left-0 fixed">
-            <ul>
-                <li className="flex justify-between items-center w-40
-                h-14 px-4 bg-gray-500  text-white
-                ml-[-100px]  hover:ml-[-5px] hover:rounded-md  duration-500
-                 ">
-                    <a href="https://www.linkedin.com/in/priyank-raiyani-702b61223"
-                       className="flex justify-between items-center w-full  text-xl">
-                        LinkedIn<FaLinkedin size={30}/>
+    return (<div className=" hidden lg:flex flex-col top-[35%] left-0 fixed">
+        <ul>
+            {Slinks.map(({id, child, href, style, download}) => (
+                <li key={id}
+                    className={"flex justify-between items-center w-40 h-14" +
+                        " px-4 bg-gray-500  text-white  ml-[-100px]  " +
+                        "hover:ml-[-5px] hover:rounded-md  duration-500 " +
+                        " " + style} >
+                    <a href={href}
+                       className="flex justify-between items-center w-full  text-xl"
+                        download={download}
+                       target="_blank"
+                       rel="noreferrer"
+                    >
+                        {child}
+
                     </a>
-                </li>
-            </ul>
-        </div>
-    );
+                </li>))}
+
+        </ul>
+    </div>);
 };
 
 export default SocialLinks;
