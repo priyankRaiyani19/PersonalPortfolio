935dbe1ad059c8dbd4191437061871615bfdea6c
priyank <priyankraiyani19@gmail.com>
2024-07-15 19:40:17 +0530

    fix bug

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,9 @@ import {Link} from "react-scroll"
 
 const Home = () => {
     return (
-        <div name="Home" className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800 border-b border-gray-500 ">
+        <div name="Home"
+             className="h-screen w-full bg-gradient-to-b from-black via-black
+              to-gray-800 border-b border-gray-500 ">
 
             <div className="max-w-screen-lg mx-auto flex flex-col
              items-center justify-between h-full px-4 md:flex-row sm:flex-row">
@@ -30,7 +32,7 @@ const Home = () => {
                         </Link>
                     </div>
                 </div>
-                <div className='flex mt-[670px] h-[1000px] w-[2500px] hidden lg:flex'>
+                <div className='flex mt-[610px] h-[1000px] w-[2500px] hidden lg:flex '>
                     <img src={HeroImg} alt="Notfound"
                          className="rounded-3xl mx-auto lg:mx-[40px]
                          lg:h-[612px] lg:w-[2500px] md:w-[990px]"/>
