fd6c248cd9a4eeb6878eb9970e197e5d995ead68
priyank <priyankraiyani19@gmail.com>
2024-07-15 11:26:26 +0530

    fix bug

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,38 +9,7 @@ module.exports = {
     },
     plugins: [],
 }
-//
-// tailwind.config.js
-// tailwind.config.js
-// tailwind.config.js
-// tailwind.config.js
-// tailwind.config.js
-// tailwind.config.js
-// tailwind.config.js
 
-// module.exports = {
-//     content: [
-//         './src/**/*.{js,jsx,ts,tsx}', // Adjust the path as necessary
-//     ],
-//     theme: {
-//         extend: {
-//             keyframes: {
-//                 glowBorder: {
-//                     '0%, 100%': {
-//                         'text-shadow': '0 0 2px #fff, 0 0 4px #fff, 0 0 6px #fff, 0 0 8px #fff, 0 0 10px #fff, 0 0 12px #fff, 0 0 14px #fff',
-//                     },
-//                     '50%': {
-//                         'text-shadow': '0 0 4px #fff, 0 0 8px #fff, 0 0 12px #fff, 0 0 16px #fff, 0 0 20px #fff, 0 0 24px #fff, 0 0 28px #fff',
-//                     },
-//                 },
-//             },
-//             animation: {
-//                 glowBorder: 'glowBorder 1.5s ease-in-out infinite alternate',
-//             },
-//         },
-//     },
-//     plugins: [],
-// };
 
 
 
